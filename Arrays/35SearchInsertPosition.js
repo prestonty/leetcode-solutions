00fef674c19a9b71e412193840db00684f8b2d0e
Preston Tom-Ying <preston.tomying@gmail.com>
2025-01-21 15:27:03 -0500

    js array

---
+++ Arrays/35SearchInsertPosition.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        // includes equal cause when they are equal, they could pinpoint/focus into the exact location
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
